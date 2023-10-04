aa8eee9b5064977428951ed4e2dc4ae94f27b6ad
Angkush <angkysahu2502@gmail.com>
2023-10-04 20:34:06 +0530

    optimised for better SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,24 +11,51 @@ In this case, I am using sessionStorage to see if I shall show the splash screen
 import dynamic from "next/dynamic";
 const Splash = dynamic(() => import("@/components/splash"), { ssr: false });
 
+const pageDescription =
+   "Hey there, I am Angkush, a dedicated penchant full-stack developer. This portfolio reflects my dedication to crafting seamless digital experiences. Let's start an amazing journey of creating exciting and user friendly applications today.";
+
 export const metadata: Metadata = {
    title: "Angkush Sahu",
-   description:
-      "Hey there, I am Angkush, a dedicated penchant full-stack developer. This portfolio reflects my dedication to crafting seamless digital experiences. Let's start an amazing journey of creating exciting and user friendly applications today.",
+   description: pageDescription,
    viewport: "width=device-width, initial-scale=1.0",
    themeColor: "#000000",
-   icons: [{ rel: "apple-touch-icon", url: "/apple-touch-icon.png" }],
-   robots: "index, follow",
-   keywords: "angkush, sahu, portfolio, website, web, developer, full, stack",
-   manifest: "site.webmanifest",
+   robots: {
+      index: true,
+      follow: true,
+      googleBot: { index: true, follow: true },
+   },
+   keywords: "angkush, sahu, portfolio, website, web, developer, full, stack, engineer",
+   manifest: "/site.webmanifest",
+   // extra SEO optimisations
+   colorScheme: "dark",
+   creator: "Angkush Sahu",
+   publisher: "Angkush Sahu",
+   applicationName: "Angkush Sahu",
+   generator: "Next.js and Vercel",
+   referrer: "origin-when-cross-origin",
+   metadataBase: new URL("https://angkushsahu.vercel.app"),
+   authors: [{ name: "Angkush Sahu", url: "https://angkushsahu.vercel.app" }],
+   openGraph: {
+      title: "Angkush Sahu",
+      description: pageDescription,
+      images: [
+         {
+            url: "/og_image.png",
+            width: 192,
+            height: 192,
+         },
+      ],
+   },
+   verification: {
+      google: "T_WCesBXsERla-yTedqacE3O1JiLe1oZSqMkARbcZ50",
+   },
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
    return (
       <html lang="en">
-         <meta name="google-site-verification" content="vPaQc-oOuZAlm6kTARCjuHjnap45EALorP_ELfBVk-Y" />
-         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
          <body>
             <main className="parent_root">
                <div className="shape_1"></div>
