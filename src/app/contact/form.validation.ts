a0733363715cdbbb250e1d68994fcd259d300cf6
Angkush Sahu <91590287+angkushsahu@users.noreply.github.com>
2024-04-26 19:06:49 +0530

    refactor: updated error message for empty email address

--- src/app/contact/form.validation.ts
+++ src/app/contact/form.validation.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const contactSchema = z.object({
    name: z.string().min(1, { message: "Please enter your name" }),
-   email: z.string().min(1, { message: "Please enter your name" }).email({ message: "Please enter a valid e-mail" }),
+   email: z.string().min(1, { message: "Please enter your e-mail" }).email({ message: "Please enter a valid e-mail" }),
    subject: z.string(),
    message: z.string().min(1, { message: "Please leave a message for me" }),
 });
